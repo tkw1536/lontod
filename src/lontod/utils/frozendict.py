ba2761fdde28d2f25e268e467c86c89953dae189
Tom Wiesing <tkw01536@gmail.com>
2025-07-03 18:31:19 +0200

    utils: Add FrozenDict class

---
+++ src/lontod/utils/frozendict.py
@@ -0,0 +1,74 @@
+"""Implements frozendict."""
+
+from collections.abc import Iterable, Iterator, Mapping
+from threading import Lock
+from typing import TypeVar, overload
+
+KT = TypeVar("KT")
+VT_co = TypeVar("VT_co", covariant=True)
+
+
+class FrozenDict(Mapping[KT, VT_co]):
+    """Like dict,but cannot be modified."""
+
+    __dict: dict[KT, VT_co]
+
+    # lots of overloads -- to be type-compatible with the standard dict
+    @overload
+    def __init__(self) -> None: ...
+    @overload
+    def __init__(self: "FrozenDict[str, VT_co]", **kwargs: VT_co) -> None: ...
+    @overload
+    def __init__(self, mp: "Mapping[KT, VT_co]", /) -> None: ...
+    @overload
+    def __init__(
+        self: "FrozenDict[str, VT_co]", mp: "Mapping[str, VT_co]", /, **kwargs: VT_co
+    ) -> None: ...
+    @overload
+    def __init__(self, iterable: Iterable[tuple[KT, VT_co]], /) -> None: ...
+    @overload
+    def __init__(
+        self: "FrozenDict[str, VT_co]",
+        iterable: Iterable[tuple[str, VT_co]],
+        /,
+        **kwargs: VT_co,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: "FrozenDict[str, str]", iterable: Iterable[list[str]], /
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: "FrozenDict[bytes, bytes]", iterable: Iterable[list[bytes]], /
+    ) -> None: ...
+
+    def __init__(self, *args, **kwargs):  # type: ignore[no-untyped-def]
+        """Create a new frozen dictionary."""
+        self.__dict = dict(*args, **kwargs)
+        self._l = Lock()
+        self._hash = None
+
+    def __iter__(self) -> Iterator[KT]:
+        """Iterate through the items in this dictionary."""
+        return iter(self.__dict)
+
+    def __getitem__(self, key: KT) -> VT_co:
+        """Get an item from this dictionary."""
+        return self.__dict[key]
+
+    def __len__(self) -> int:
+        """Total number of items in this dictionary."""
+        return len(self.__dict)
+
+    _l: Lock
+    """protect _hash"""
+
+    _hash: int | None
+    """holds a cached hash"""
+
+    def __hash__(self) -> int:
+        """Compute the hash of the underlying items."""
+        with self._l:
+            if self._hash is None:
+                self._hash = hash(tuple(sorted(self.__dict.items())))
+            return self._hash
---
+++ tests/lontod/utils/test_frozendict.py
@@ -0,0 +1,63 @@
+"""Tests the frozendict module."""
+
+from typing import Any
+
+import pytest
+
+from lontod.utils.frozendict import FrozenDict
+
+
+@pytest.mark.parametrize(
+    "md",
+    [
+        {},
+        {"hello": "world"},
+        {"a": 1, "b": 2},
+        {1.0: "a", 2.0: "b"},
+    ],
+)
+def test_frozen_dict_behavior(md: dict[Any, Any]) -> None:
+    """Tests behavior of a frozen dict."""
+    fd = FrozenDict(md)
+
+    assert fd is not md
+
+    # check that keys are identical
+    assert tuple(fd.keys()) == tuple(md.keys())
+
+    # check that items are identical
+    assert tuple(fd.items()) == tuple(md.items())
+
+    # check that len is identical
+    assert len(fd) == len(md)
+
+    # check that the values are identical
+    for key, value in md.items():
+        assert fd[key] == value
+
+
+def test_frozen_dict_equality() -> None:
+    """Tests equality of the frozen dict class."""
+    # FrozenDicts are expected to be ==-equal within a group
+    # and different between groups.
+    groups: list[list[FrozenDict[Any, Any]]] = [
+        [
+            FrozenDict(hello="world", bye="universe"),
+            FrozenDict(hello="world", bye="universe"),
+            FrozenDict(bye="universe", hello="world"),
+        ],
+        [FrozenDict(other="world")],
+        [FrozenDict(other=12)],
+    ]
+
+    for group_index, group in enumerate(groups):
+        for elem_index, elem in enumerate(group):
+            for inner_group_index, inner_group in enumerate(groups):
+                for inner_elem_index, inner_elem in enumerate(inner_group):
+                    # they are ==-equal iff they are in the same group
+                    assert (elem == inner_elem) == (inner_group_index == group_index)
+                    # they are is-equal iff they are in the same group and have the same index
+                    assert (elem is inner_elem) == (
+                        inner_group_index == group_index
+                        and inner_elem_index == elem_index
+                    )
