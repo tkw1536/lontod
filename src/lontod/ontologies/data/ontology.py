84852671fa5ee3b785a7469a06d1d2e79c0de70f
Tom Wiesing <tkw01536@gmail.com>
2025-10-31 13:44:48 +0100

    html: Show permanent links to definenda on hover

--- src/lontod/ontologies/data/ontology.py
+++ src/lontod/ontologies/data/ontology.py
@@ -66,6 +66,7 @@ class Definiendum(_DefiniendumLike, HTMLable):
 
     @override
     def to_html(self, ctx: RenderContext) -> NodeLike:
+        fragment = ctx.fragment(self.iri)
         return DIV(
             H3(
                 SPAN(CODE(ctx.format_iri(self.iri))),
@@ -74,6 +75,12 @@ class Definiendum(_DefiniendumLike, HTMLable):
                     self.prop.abbrev,
                     _class=f"sup-{self.prop.abbrev}",
                     title=self.prop.inline_title,
+                ),
+                A(
+                    href="#" + fragment,
+                    aria_current="location",
+                    rel="noreferrer noopener",
+                    _class="permalink",
                 ),
             ),
             TABLE(
@@ -86,7 +93,7 @@ class Definiendum(_DefiniendumLike, HTMLable):
                     for (prop, resources) in self.properties.items()
                 ),
             ),
-            id=ctx.fragment(self.iri),
+            id=fragment,
             _class="property entity",
         )
 
